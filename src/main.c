d9462a3a55b858e9087e7cf042c8f25ecbb8347b
flmarsou <marsouinflorent12@gmail.com>
2024-10-03 19:40:31 +0200

    Added Struct Init & Minor changes

--- src/main.c
+++ src/main.c
@@ -1,15 +1,27 @@
 #include "../includes/ftop.h"
 
+void    init_struct(t_cpu *cpu)
+{
+    cpu->model[0] = 'N';
+    cpu->model[1] = '/';
+    cpu->model[2] = 'A';
+    cpu->model[3] = '\0';
+    cpu->frequency = 0;
+    cpu->threads = 0;
+}
+
 int main(void)
 {
     t_cpu   cpu;
 
+    init_struct(&cpu);
     if (!cpu_info(&cpu))
         return (1);
+    
+    initscr();
 
-    printf("CPU Model:                %s\n", cpu.model);
-    printf("Frequency:                %d\n", cpu.frequency);
-    printf("Number of Threads:        %d\n", cpu.threads);
+    cpu_menu(cpu);
 
+    endwin();
     return (0);
 }
