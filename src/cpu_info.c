7d89b42a904b03acdcb85aaeec1ee5230e7625a9
flmarsou <marsouinflorent12@gmail.com>
2024-10-06 20:27:00 +0200

    TODO: Buffer overflow

--- src/cpu_info.c
+++ src/cpu_info.c
@@ -1,10 +1,10 @@
 #include "../includes/ftop.h"
 
-bool    cpu_info(t_cpu *cpu)
+bool    cpu_info(t_info *info)
 {
     int     fd;
     ssize_t bytesRead;
-    char    buffer[256];
+    char    buffer[10000];
     char    *targetStart;
     char    *targetEnd;
 
@@ -28,8 +28,8 @@ bool    cpu_info(t_cpu *cpu)
             {
                 if (targetEnd - (strchr(targetStart, ':') + 2) <= 63)
                 {
-                    strncpy(cpu->model, strchr(targetStart, ':') + 2, targetEnd - (strchr(targetStart, ':') + 2));
-                    cpu->model[targetEnd - (strchr(targetStart, ':') + 2)] = '\0';
+                    strncpy(info->cpu.model, strchr(targetStart, ':') + 2, targetEnd - (strchr(targetStart, ':') + 2));
+                    info->cpu.model[targetEnd - (strchr(targetStart, ':') + 2)] = '\0';
                 }
             }
         }
@@ -39,7 +39,7 @@ bool    cpu_info(t_cpu *cpu)
         {
             if ((targetEnd = strchr(targetStart, '\n')) != NULL)
             {
-                cpu->frequency = strtol(strchr(targetStart, ':') + 2, NULL, 10);
+                info->cpu.frequency = strtol(strchr(targetStart, ':') + 2, NULL, 10);
             }
         }
 
@@ -48,12 +48,12 @@ bool    cpu_info(t_cpu *cpu)
         {
             if ((targetEnd = strchr(targetStart, '\n')) != NULL)
             {
-                cpu->threads = strtol(strchr(targetStart, ':') + 2, NULL, 10);
+                info->cpu.threads = strtol(strchr(targetStart, ':') + 2, NULL, 10);
             }
         }
 
         // Ends the loop once all information has been copied.
-        if (cpu->model[0] != '\0' && cpu->frequency && cpu->threads)
+        if (info->cpu.model[0] != '\0' && info->cpu.frequency && info->cpu.threads)
             break ;
         else
             return (false);
@@ -67,38 +67,4 @@ bool    cpu_info(t_cpu *cpu)
     }
     close(fd);
     return (true);
-}
-
-void    cpu_menu(t_cpu cpu)
-{
-    // Creates the menu window.
-    WINDOW  *menu = newwin(0, 30, 0, 0);
-    box(menu, 0, 0);
-    
-    // Title
-    wattron(menu, A_BOLD);
-    mvwprintw(menu, 1, 1, "       CPU Detected:");
-    wattroff(menu, A_BOLD);
-
-    // Print CPU Model (with left padding)
-    char            *token = strtok(cpu.model, " ");
-    unsigned int    token_size;
-    unsigned int    x = 6;
-    unsigned int    y = 2;
-    unsigned int    padding = x;
-
-    while (token != NULL)
-    {
-        token_size = strlen(token);
-        if (x + token_size >= 30)
-        {
-            x = padding;
-            y++;
-        }
-        mvwprintw(menu, y, x, "%s", token);
-        x += token_size + 1;
-        token = strtok(NULL, " ");
-    }
-
-    wgetch(menu);
 }
