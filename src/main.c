33ef32ba5a339bd55683ae969570a8af74041b45
flmarsou <marsouinflorent12@gmail.com>
2024-10-06 20:25:45 +0200

    Added Resizing and Main Loop

--- src/main.c
+++ src/main.c
@@ -1,26 +1,64 @@
 #include "../includes/ftop.h"
 
-void    init_struct(t_cpu *cpu)
+t_info          info;
+unsigned int    xMax;
+unsigned int    yMax;
+
+void    handle_resize(int sig)
 {
-    cpu->model[0] = 'N';
-    cpu->model[1] = '/';
-    cpu->model[2] = 'A';
-    cpu->model[3] = '\0';
-    cpu->frequency = 0;
-    cpu->threads = 0;
+    (void)sig;
+
+    endwin();
+    refresh();
+    clear();
+
+    getmaxyx(stdscr, yMax, xMax);
+
+    cpu_menu(info, xMax, yMax);
+}
+
+void    init_struct(t_info *info)
+{
+    info->cpu.model[0] = 'N';
+    info->cpu.model[1] = '/';
+    info->cpu.model[2] = 'A';
+    info->cpu.model[3] = '\0';
+    info->cpu.frequency = 0;
+    info->cpu.threads = 0;
+
+    info->layout.selected = 0;
+    info->layout.checked[0] = 1;
+    info->layout.checked[1] = 1;
+    info->layout.checked[2] = 1;
 }
 
 int main(void)
 {
-    t_cpu   cpu;
-
-    init_struct(&cpu);
-    if (!cpu_info(&cpu))
+    // Initializes structs
+    init_struct(&info);
+    if (!cpu_info(&info))
+    {
         return (1);
-    
-    initscr();
+    }
 
-    cpu_menu(cpu);
+    // Initializes ncurses
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, true);
+
+    // Resizing
+    signal(SIGWINCH, handle_resize);
+    handle_resize(0);
+
+    // Core Loop
+    while (1)
+    {
+        cpu_menu(info, xMax, yMax);
+        handle_input(&info);
+        usleep(10000);
+    }
 
     endwin();
     return (0);
